1d065e9d8124f9c7a889a3ba2d20a93b14cbd09a
Matthias Langhard <matthias.langhard@ecologic.ch>
2017-06-06 13:42:04 +0200

    adds notes feature to app

--- src/pages/beer-detail/beer-detail.component.ts
+++ src/pages/beer-detail/beer-detail.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {NavParams} from 'ionic-angular';
+import {NavParams, ToastController} from 'ionic-angular';
 import {BeerService} from "../../services/beer.service";
 import {FavService} from "../../services/fav.service";
+import {FormControl} from "@angular/forms";
 
 @Component({
     selector: 'beer-detail',
@@ -12,10 +13,15 @@ export class BeerDetailComponent implements OnInit {
     id: string;
     isFavorite: boolean;
     isLoading: boolean = true;
+    imageHasLoaded: boolean;
+    note: string;
+    hasNote: boolean;
+    noteControl = new FormControl();
 
     constructor(private beerService: BeerService,
                 private favService: FavService,
-                params: NavParams) {
+                params: NavParams,
+                private toastCtrl: ToastController) {
         this.id = params.data.item.id;
     }
 
@@ -26,6 +32,48 @@ export class BeerDetailComponent implements OnInit {
                 this.isLoading = false;
             });
         this.checkIfIsFavorite();
+
+        this.getNoteForBeer();
+
+        // debounce keystroke events on the note textarea
+        this.noteControl.valueChanges
+            .debounceTime(1500)
+            .subscribe(newValue => {
+                this.note = newValue;
+                if (!this.hasNote) {
+                    this.favService.createNote({id: this.id, note: this.note})
+                        .subscribe(() => {
+                            this.presentNoteSavedMessage();
+                        });
+                } else {
+                    this.favService.updateNote({id: this.id, note: this.note})
+                        .subscribe(() => {
+                            this.presentNoteSavedMessage();
+                        });
+                }
+            });
+
+    }
+
+    imageLoaded() {
+        this.imageHasLoaded = true;
+    }
+
+    getNoteForBeer() {
+        this.favService.getNoteForBeer(this.id)
+            .subscribe((result) => {
+                this.note = result.note;
+                this.hasNote = result.note;
+            });
+    }
+
+    presentNoteSavedMessage() {
+        let toast = this.toastCtrl.create({
+            message: 'Notitz gespeichert',
+            duration: 3000,
+            cssClass: "toast",
+        });
+        toast.present();
     }
 
     toggleFavorite() {
--- src/services/fav.service.ts
+++ src/services/fav.service.ts
@@ -6,13 +6,13 @@ import {Observable} from "rxjs";
 @Injectable()
 export class FavService {
     CONNECTION_ERROR: string = "Es wurde ein Problem bei der Datenverbindung festegestellt. Bitte versuchen Sie es erneut.";
-    baseUrl: string = "http://localhost:3000/favorites";
+    baseUrl: string = "http://localhost:3000";
 
     constructor(private http: Http) {
     }
 
     getAllFavorites(): Observable<any[]> {
-        let query = `${this.baseUrl}`;
+        let query = `${this.baseUrl}/favorites`;
         console.log(`GET: ${query}`);
         return this.http.get(query)
             .map((res: Response) => res.json())
@@ -20,7 +20,7 @@ export class FavService {
     }
 
     addFavorite(beer: any): Observable<any> {
-        let query = `${this.baseUrl}`;
+        let query = `${this.baseUrl}/favorites`;
         console.log(`POST: ${query}`);
         return this.http.post(query, beer)
             .map((res: Response) => res.json())
@@ -28,7 +28,7 @@ export class FavService {
     }
 
     deleteFavorite(id: string): Observable<any> {
-        let query = `${this.baseUrl}/${id}`;
+        let query = `${this.baseUrl}/favorites/${id}`;
         console.log(`DELETE: ${query}`);
         return this.http.delete(query)
             .map((res: Response) => res.json())
@@ -39,5 +39,40 @@ export class FavService {
         const errMsg = (error.message) ? error.message : error.status ? `${error.status} - ${error.statusText}` : this.CONNECTION_ERROR;
         console.error(errMsg);
         return Observable.throw(errMsg);
+    }
+
+    createNote(note: any) {
+        console.log("Create Note");
+        let query = `${this.baseUrl}/notes`;
+        console.log(`POST: ${query}`);
+        return this.http.post(query, note)
+            .map((res: Response) => res.json())
+            .catch(this.handleError.bind(this));
+    }
+
+    updateNote(note: any): Observable<any> {
+        if (note.note === "") {
+            console.log("Delete Note");
+            let query = `${this.baseUrl}/notes/${note.id}`;
+            console.log(`DELETE: ${query}`);
+            return this.http.delete(query)
+                .map((res: Response) => res.json())
+                .catch(this.handleError.bind(this));
+        } else {
+            console.log("Update Note");
+            let query = `${this.baseUrl}/notes/${note.id}`;
+            console.log(`PUT: ${query}`);
+            return this.http.put(query, note)
+                .map((res: Response) => res.json())
+                .catch(this.handleError.bind(this));
+        }
+    }
+
+    getNoteForBeer(id: string): Observable<any> {
+        let query = `${this.baseUrl}/notes/${id}`;
+        console.log(`GET: ${query}`);
+        return this.http.get(query)
+            .map((res: Response) => res.json())
+            .catch(() => "No Note for Beer...");
     }
 }
